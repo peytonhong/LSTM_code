331f3ac3833440117a5b4e64e08028cf6c21d457
Hong, Hyosung <hhs8314@gmail.com>
2017-12-31 15:23:25 +0900

    revision

--- LSTMAutoenPred-1.py
+++ LSTMAutoenPred-1.py
@@ -19,13 +19,13 @@ class LSTMAutoencoder(object):
     cell=None, optimizer=None, reverse=True, 
     decode_without_input=True):
 
-    self.batch_num = list(inputs[0].shape)[0]
-    self.elem_num = list(inputs[0].shape)[1]
+    self.batch_num = inputs[0].shape[0]
+    self.elem_num  = inputs[0].shape[1]
 
     if cell is None:
-      self._enc_cell = BasicLSTMCell(hidden_num, state_is_tuple=True)
-      self._dec_cell = BasicLSTMCell(hidden_num, state_is_tuple=True)
-      self._pred_cell = BasicLSTMCell(hidden_num, state_is_tuple=True)
+      self._enc_cell  = BasicLSTMCell(hidden_num)
+      self._dec_cell  = BasicLSTMCell(hidden_num)
+      self._pred_cell = BasicLSTMCell(hidden_num)
       
     else :
       self._enc_cell = cell
@@ -33,11 +33,11 @@ class LSTMAutoencoder(object):
       self._pred_cell = cell
 
     with tf.variable_scope('encoder'):
-        enc_state = (tf.zeros([self.batch_num,hidden_num]), tf.zeros([self.batch_num,hidden_num]))
+        enc_state = (tf.zeros([self.batch_num,hidden_num]), tf.zeros([self.batch_num,hidden_num])) # (cell state, hidden state) tuple
         self.z_codes = []
         inputs_tf=tf.convert_to_tensor(inputs)
         for step in range(len(inputs)):
-          enc_output, enc_state = self._enc_cell(inputs_tf[step], enc_state)
+          enc_output, enc_state = self._enc_cell.call(inputs_tf[step], enc_state) # .call(input, state) : calls LSTM, otherwise RNN.
           self.z_codes.append(enc_output)
         self.enc_state = enc_state
         print(self.z_codes)
@@ -57,7 +57,7 @@ class LSTMAutoencoder(object):
         dec_outputs = []
 
         for step in range(len(inputs)):
-          dec_output, dec_state = self._dec_cell(dec_inputs[step], dec_state)
+          dec_output, dec_state = self._dec_cell.call(dec_inputs[step], dec_state)
           dec_outputs.append(dec_output)
 
         if reverse:
@@ -95,7 +95,7 @@ class LSTMAutoencoder(object):
       pred_outputs = []
       for step in range(len(inputs)):
         if step>0: vs1.reuse_variables()
-        pred_input_, pred_state = self._pred_cell(pred_input_, pred_state)
+        pred_input_, pred_state = self._pred_cell.call(pred_input_, pred_state)
         pred_input_ = tf.matmul(pred_input_, pred_weight_) + pred_bias_
         pred_outputs.append(pred_input_)
       self.predoutput_ = tf.transpose(tf.stack(pred_outputs), [1,0,2])       
@@ -110,11 +110,14 @@ class LSTMAutoencoder(object):
     else :
       self.train = optimizer.minimize(self.loss)
 
-inputs=np.array([[[1.],[4.]],[[2.],[5.]],[[3.],[6.]]],dtype=np.float32)
-testX=np.array([[[4.],[5.]],[[5.],[6.]],[[6.],[7.]]],dtype=np.float32)
-
-predinputs=np.array([[[-2.],[1.]],[[-1.],[2.]],[[0.],[3.]]],dtype=np.float32)
-predtestX=np.array([[[1.],[2.]],[[2.],[3.]],[[3.],[4.]]],dtype=np.float32)
+#inputs=np.array([[[1.],[4.]],[[2.],[5.]],[[3.],[6.]]],dtype=np.float32)
+#testX=np.array([[[4.],[5.]],[[5.],[6.]],[[6.],[7.]]],dtype=np.float32)
+#predinputs=np.array([[[-2.],[1.]],[[-1.],[2.]],[[0.],[3.]]],dtype=np.float32)
+#predtestX=np.array([[[1.],[2.]],[[2.],[3.]],[[3.],[4.]]],dtype=np.float32)
+inputs = np.array([[[1],[2],[3]],[[4],[5],[6]]], dtype=np.float32)
+testX = np.array([[[2],[3],[4]],[[3],[4],[5]]], dtype=np.float32)
+predinputs = np.array([[[4],[5],[6]],[[7],[8],[9]]], dtype=np.float32)
+predtestX = np.array([[[3],[4],[5]],[[4],[5],[6]]], dtype=np.float32)
 
 ae = LSTMAutoencoder(4, inputs, predinputs)
 with tf.Session() as sess:
